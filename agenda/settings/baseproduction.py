5d9d8c32658cf01b11380104ade65ccbc1656734
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2022-04-03 21:31:13 -0300

    Ajustes Base

--- agenda/settings/baseproduction.py
+++ agenda/settings/baseproduction.py
@@ -131,8 +131,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
