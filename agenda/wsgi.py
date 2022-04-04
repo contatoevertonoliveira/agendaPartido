2e51aa84c1aa4581e5c7f6c8d39d9baa947c07f5
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2022-04-04 17:22:52 -0300

    Ajuste Base

--- agenda/wsgi.py
+++ agenda/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 import os
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agenda.settings.base')
 
-from django.core.wsgi import get_wsgi_application
-# from whitenoise.django import DjangoWhiteNoise
-application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+from whitenoise.django import DjangoWhiteNoise
+# application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
