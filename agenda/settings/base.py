ee0bcd826111f3a5206348d2ded6756cc7798616
Everton Oliveira <contato.evertonoliveira@hotmail.com>
2022-04-04 22:41:20 -0300

    Ajuste Base

--- agenda/settings/base.py
+++ agenda/settings/base.py
@@ -148,9 +148,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,  'static'),
-)
+STATICFILES_DIRS = None
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,  'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
